24a227f2a1b9831b48a528ee862bc74b8b7a23fc
Ro-Dunn <minutetoominute@gmail.com>
2022-05-11 08:26:30 -0600

    set up for the game- non functioning

--- Text Adventure/ViewControllers/PlayScreenViewController.swift
+++ Text Adventure/ViewControllers/PlayScreenViewController.swift
@@ -11,10 +11,10 @@ import UIKit
 class PlayScreenViewController: UIViewController{
     
     var leftButtonCount:Int = 0
-
+    
     var rightButtonCount:Int = 0
-
-    var skipButtonCount:Int = 1
+    
+    var skipButtonCount:Int = 0
     
     var fantasyAllowed = false
     
@@ -37,6 +37,29 @@ class PlayScreenViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if fantasyAllowed == true{
+            storyTextView.text = introText1
+            playFantasyStory()
+            
+        } else if sciFiAllowed == true{
+            storyTextView.text = sciFiLine1
+            playSciFiStory()
+            
+        } else if westernAllowed == true{
+            storyTextView.text = "kjasfjb"
+            playWesternStory()
+        }
+        
+        //If skip/next is an option then right and left shouldn't be
+        if skipButton.isEnabled == true{
+            leftButton.isEnabled = false
+            rightButton.isEnabled = false
+        }
+        
+        if skipButtonCount == 0{
+            skipButton.backgroundColor = .gray
+            skipButton.isEnabled = false
+        }
     }
 
     
@@ -47,25 +70,102 @@ class PlayScreenViewController: UIViewController{
     
     @IBAction func leftButtonTapped(_ sender: Any) {
         leftButtonCount = leftButtonCount + 1
-        
+        buttonCheck()
     }
     
     
     @IBAction func rightButtonTapped(_ sender: Any) {
         rightButtonCount = rightButtonCount + 1
-        
+        buttonCheck()
     }
     
     
     @IBAction func skipButtonTapped(_ sender: Any) {
         skipButtonCount = skipButtonCount - 1
-        if skipButtonCount == 0{
-            skipButton.backgroundColor = .gray
-            //disable skipButton
-        }
-        
+        buttonCheck()
     }
     
     
     
+    func buttonCheck(){
+        playSciFiStory()
+    }
+    
+    
+    
+    
+    func playFantasyStory(){
+        
+    }
+    
+    func playWesternStory(){
+        
+    }
+    
+    func playSciFiStory(){
+        skipButtonCount = 1
+        print("here")
+        skipButton.setTitle("NEXT", for: .normal)
+        if skipButtonCount == 0{
+            print("here")
+            storyTextView.text = sciFiline1AndAHalf
+            leftButton.setTitle("RETURN TO POD", for: .normal)
+            rightButton.setTitle("WALK AROUND", for: .normal)
+            //return to pod
+            if leftButtonCount == 1{
+                storyTextView.text = sciFiLine2
+                skipButtonCount = skipButtonCount + 1
+                skipButton.setTitle("YOU DIED", for: .normal)
+                if skipButtonCount == 0{
+                    self.performSegue(withIdentifier: "endingSegue", sender: self)
+                }
+            }
+            //walk around
+            if rightButtonCount == 1{
+                storyTextView.text = sciFiLine3
+                leftButton.setTitle("LET'S GRAB A COFFEE", for: .normal)
+                rightButton.setTitle("I WANNA SEE MORE OF THE SHIP", for: .normal)
+                //grab a coffee
+                if leftButtonCount == 1{
+                    storyTextView.text = sciFiLine4
+                    leftButton.setTitle("BREAK AWAY", for: .normal)
+                    rightButton.setTitle("LISTEN TO HIM", for: .normal)
+                    //break away
+                    if leftButtonCount == 2{
+                        storyTextView.text = sciFiLine5
+                        skipButtonCount = skipButtonCount + 1
+                        skipButton.setTitle("YOU DIED", for: .normal)
+                        if skipButtonCount == 0{
+                            self.performSegue(withIdentifier: "endingSegue", sender: self)
+                        }
+                    }
+                }
+                //listen to him
+                if rightButtonCount == 2{
+                    storyTextView.text = sciFiLine6
+                    skipButtonCount = skipButtonCount + 2
+                    if skipButtonCount == 1{
+                        storyTextView.text = sciFiline6AndAHalf
+                        skipButton.setTitle("YOU SURVIVED", for: .normal)
+                        if skipButtonCount == 0{
+                            self.performSegue(withIdentifier: "endingSegue", sender: self)
+                        }
+                    }
+                }
+                //I wanna see more of the ship
+                if rightButtonCount == 2{
+                    storyTextView.text = sciFiLine7
+                    skipButtonCount = skipButtonCount + 2
+                    if skipButtonCount == 1{
+                        storyTextView.text = sciFiLine8
+                        skipButton.setTitle("YOU SURVIVED", for: .normal)
+                        if skipButtonCount == 0 {
+                            //unwind
+                            self.performSegue(withIdentifier: "endingSegue", sender: self)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
