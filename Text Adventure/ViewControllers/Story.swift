3807de67dac9565529a252d1cf6d647952af052f
Jeremy0124 <Jeremy.0124@yahoo.com>
2022-06-07 11:21:00 -0600

    updated Fantasy

--- Text Adventure/ViewControllers/Story.swift
+++ Text Adventure/ViewControllers/Story.swift
@@ -5,45 +5,12 @@
 //  Created by Ro Dunn on 5/16/22.
 //
 
-//import Foundation
-//
-//class Story {
-//    //sets the title and holds everything
-//    var title: String
-//    var text: String
-//    var choices = [Choice]()
-//    var isFinished = false
-//
-//    init(title: String, text: String, choices: [Choice] = []) {
-//        self.title = title
-//        self.text = text
-//        self.choices = choices
-//    }
-//
-//    func addChoice(_ choice: Choice) {
-//        choices.append(choice)
-////        text.append(choice.text)
-//    }
-//
-////    func chooseLeft() {
-////        guard let currentChoice = choices.last else { return }
-////        text.append(currentChoice.leftStoryText)
-////        if let leftChoiceId = currentChoice.leftNextChoiceId, let nextChoice = findTheNExtChoice {
-////            addChoice(nextChoice)
-////        } else {
-////            isFinished = true
-////        }
-////    }
-//
-////    func chooseRight() {
-////        guard let currentChoice = choices.last else { return }
-////        text.append(currentChoice.rightStoryText)
-////        if let rightChoiceId = currentChoice.rightNextChoiceId, let nextChoice = findTheNExtChoice {
-////            addChoice(nextChoice)
-////        } else {
-////            isFinished = true
-////        }
-////    }
-//
-//}//end
-//
+import Foundation
+import UIKit
+
+struct Story {
+    var title: String
+       var image: String
+       var music: String
+       var startNode: ChoiceNode
+}
