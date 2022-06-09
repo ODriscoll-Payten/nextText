99298daf7a2ed03c3c297a5227301da8431350f2
Ro-Dunn <minutetoominute@gmail.com>
2022-06-09 11:11:44 -0600

    spaces

--- Text Adventure/ViewControllers/EndScreenViewController.swift
+++ Text Adventure/ViewControllers/EndScreenViewController.swift
@@ -9,17 +9,17 @@ import Foundation
 import UIKit
 
 class EndScreenViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
-
+    
     
     
     @IBAction func ChooseAnotherStoryPressed(_ sender: Any) {
-      
-        }
+        
     }
+}
 
