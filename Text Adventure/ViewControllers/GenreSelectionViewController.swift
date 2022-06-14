a4b5e69fa714d1ce1d8cfffed849f5e68b9f2af5
Ro-Dunn <minutetoominute@gmail.com>
2022-06-14 10:13:49 -0600

    part1

--- Text Adventure/ViewControllers/GenreSelectionViewController.swift
+++ Text Adventure/ViewControllers/GenreSelectionViewController.swift
@@ -11,44 +11,28 @@ import AVFoundation
 
 class GenreSelectionViewController: UIViewController {
     
-    var shouldTutorialShow = false
-    
     @IBOutlet weak var choseTutorialButton: UIButton!
     
     @IBOutlet weak var choseFantasyGenreButton: UIButton!
     @IBOutlet weak var choseWesternGenreButton: UIButton!
     @IBOutlet weak var choseSciFiGenreButton: UIButton!
     
+    var shouldTutorialShow = true
 
     //    var backGroundPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
-  
-//        choseFantasyGenreButton.layer.cornerRadius = 25
-//        choseSciFiGenreButton.layer.cornerRadius = 25
-//        choseWesternGenreButton.layer.cornerRadius = 25
-        if shouldTutorialShow == true{
-            choseFantasyGenreButton.isEnabled = false
-            choseWesternGenreButton.isEnabled = false
-            choseSciFiGenreButton.isEnabled = false
-            
-        } else if shouldTutorialShow == false{
-            choseFantasyGenreButton.isEnabled = true
-            choseWesternGenreButton.isEnabled = true
-            choseSciFiGenreButton.isEnabled = true
-        }
-        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     
     
     @IBAction func unwindToGenreSelection(_ unwindSegue: UIStoryboardSegue) {
         let sourceViewController = unwindSegue.source
         // Use data from the view controller which initiated the unwind segue
     }
-    
-    
+
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let playVC = segue.destination as? PlayScreenViewController else { return}
@@ -65,6 +49,9 @@ class GenreSelectionViewController: UIViewController {
             playVC.currentStory = tutorial
             shouldTutorialShow = false
             UserDefaults.standard.set(shouldTutorialShow, forKey: "tutorialOn?")
+            choseTutorialButton.isEnabled = false
+            choseTutorialButton.isHidden = true
+            
         }
     }
     
